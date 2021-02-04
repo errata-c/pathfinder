6b02d15fa7142d0ba63762ab70ce7d7d2fc3200d
errata-c <77643526+errata-c@users.noreply.github.com>
2021-02-04 14:49:35 -0500

    Fixed shared library linkage

--- pathfinder-config.cmake
+++ pathfinder-config.cmake
@@ -23,22 +23,23 @@ set(pathfinder_CONFIG_DIR ${PACK_CONFIG_DIR})
 mark_as_advanced(pathfinder_VERSION pathfinder_PREFIX_DIR pathfinder_ROOT_DIR pathfinder_CONFIG_DIR)
 
 # Create imported target ez-window
-add_library(pathfinder SHARED IMPORTED)
+add_library(pathfinder::pathfinder SHARED IMPORTED)
 
-set_target_properties(pathfinder PROPERTIES
+set_target_properties(pathfinder::pathfinder PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${PACKAGE_PREFIX_DIR}/include"
+  INTERFACE_COMPILE_DEFINITIONS "PATHFINDER_RESOURCES_PATH=\"${PACKAGE_PREFIX_DIR}/share/resources\""
 )
 
-set_property(TARGET pathfinder APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(pathfinder PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/bin/pathfinder.dll"
-  IMPORTED_IMPLIB_RELEASE "${PACKAGE_PREFIX_DIR}/lib/pathfinderd.lib"
+set_property(TARGET pathfinder::pathfinder APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pathfinder::pathfinder PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${PACKAGE_PREFIX_DIR}/bin/release/pathfinder.dll"
+  IMPORTED_IMPLIB_RELEASE "${PACKAGE_PREFIX_DIR}/lib/release/pathfinder.dll.lib"
   )
   
-set_property(TARGET pathfinder APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-set_target_properties(pathfinder PROPERTIES
-  IMPORTED_LOCATION_DEBUG "${PACKAGE_PREFIX_DIR}/bin/pathfinderd.dll"
-  IMPORTED_IMPLIB_DEBUG "${PACKAGE_PREFIX_DIR}/lib/pathfinderd.lib"
+set_property(TARGET pathfinder::pathfinder APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pathfinder::pathfinder PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${PACKAGE_PREFIX_DIR}/bin/debug/pathfinder.dll"
+  IMPORTED_IMPLIB_DEBUG "${PACKAGE_PREFIX_DIR}/lib/debug/pathfinder.dll.lib"
   )
 
 unset(PACK_NAME)
